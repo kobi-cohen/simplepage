749da666794209a16ea6e26b78743cfbd167bd35
kobi <kobimagic@gmail.com>
2018-11-08 07:03:51 +0200

    this is the first upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let a=0;
+let b=document.getElementById("first");
+
+//click event
+b.addEventListener("click",()=>{
+    a++;
+    
+    console.log(`You clicked on the button , this is you push number ${a}`);
+    if (a%2==0){
+        console.log("and he is zugi");
+        
+    }
+}); 
+
+//Mouse over event
+
+b.addEventListener("mouseover",(event)=>{
+    console.log("Mouse was over on the Button")
+    event.target.style.color = "orange";   
+});
+
+//Mouse out event
+
+b.addEventListener("mouseout",(event)=>{
+    event.target.style.color = "black";   
+});
